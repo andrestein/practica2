7b5c3e549f0faddc447dcb835e54cbbe7bf89b66
audoban <audoban@openmailbox.org>
2015-10-13 20:47:44 -0500

    Matriz de adyacencia

--- Grafos/src/octographs/Proceso.java
+++ Grafos/src/octographs/Proceso.java
@@ -11,42 +11,28 @@ import java.util.ArrayList;
  *
  * @author Sebastian Anderson
  */
-
-
-
-
 public class Proceso {
-    
+
     public static final int CERO = 0;
     public static final int UNO = 1;
-    
+
     // fila, columna 0: nombres de elementos 
-    public int [][] matrizAdyasencia;
-    
-    
-    public Proceso (ArrayList<Relacion> relaciones, ArrayList<Elemento> elementos){
-        ArrayList<String> temp = new ArrayList<String>();       
-        matrizAdyasencia = new int[elementos.size()+1][elementos.size()+1];       
-        for(int j = 1; j <= temp.size();j++){
-           for(int k = 1; k <= temp.size();k++){
-               matrizAdyasencia[j][k] = CERO;               
-           }
-        }       
-        for(int i = 0; i<relaciones.size();i++){
-        Relacion x =  relaciones.get(i);       
-       
-            int j = 0;
-            int f = 0;
-            int c = 0;
-            for(; j < matrizAdyasencia.length;j++){                
-                f = j;
+    public int[][] matrizAdyacencia;
+
+    public Proceso(ArrayList<Relacion> relaciones, ArrayList<Elemento> elementos) {
+
+        matrizAdyacencia = new int[elementos.size()][elementos.size()];
+        for (int i = 0; i < elementos.size(); i++) {
+            for (int j = 0; j < elementos.size(); j++) {
+                matrizAdyacencia[i][j] = CERO;
             }
-            int m = 0;
-            for(; m < matrizAdyasencia.length;m++){               
-                c = m;
-            }
-            matrizAdyasencia[f][c] = UNO;  
         }
-       
-    } 
+
+        for (int i = 0; i < relaciones.size(); i++) {
+            Relacion x = relaciones.get(i);
+            int a = elementos.indexOf(x.getElemento1());
+            int b = elementos.indexOf(x.getElemento2());
+            matrizAdyacencia[a][b] = UNO;
+        }
+    }
 }
